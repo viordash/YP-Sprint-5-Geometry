4c2e8c1eeca88dd5307c9248b8f15a764d9d1957
Vitalii Orazov <viordash@yandex.ru>
2025-08-31 09:36:46 +0000

    StackForGrahamScan: required attributes and specifiers

--- include/convex_hull.hpp
+++ include/convex_hull.hpp
@@ -11,14 +11,14 @@ double CrossProduct(Point2D p1, Point2D middle, Point2D p2);
 
 class StackForGrahamScan {
 public:
-    void Push(const Point2D &p) { s.push_back(p); }
-    void Pop() { s.pop_back(); }
+    void Push(const Point2D &p) noexcept { s.push_back(p); }
+    void Pop() noexcept { s.pop_back(); }
 
-    size_t Size() { return s.size(); }
-    Point2D Top() { return s.back(); }
-    Point2D NextToTop() { return *std::prev(s.end(), 2); }
+    [[nodiscard]] size_t Size() const noexcept { return s.size(); }
+    [[nodiscard]] const Point2D &Top() const noexcept { return s.back(); }
+    [[nodiscard]] const Point2D &NextToTop() const noexcept { return *std::prev(s.end(), 2); }
 
-    std::vector<Point2D> &&Extract() && { return std::move(s); }
+    [[nodiscard]] std::vector<Point2D> &&Extract() && noexcept { return std::move(s); }
 
 private:
     std::vector<Point2D> s;
